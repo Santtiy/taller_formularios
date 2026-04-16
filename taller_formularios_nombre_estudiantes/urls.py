6574e81333f57ca1005628b706d127758bed0462
Santtiy <santiago.bustos@campusucc.edu.co>
2026-04-16 11:33:21 -0500

    feat(home): agregar pagina de inicio con acceso a asistencia y solicitudes

--- taller_formularios_nombre_estudiantes/urls.py
+++ taller_formularios_nombre_estudiantes/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import home
+
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('asistencia/', include('asistencia.urls')),
     path('solicitudes/', include('solicitudes.urls')),
